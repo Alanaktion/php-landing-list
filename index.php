bcb8a37d083fb0916c1dfb38da2fbc9ad7fb5019
Alan Hardman <alanaktion@gmail.com>
2014-07-25 14:53:17 -0600

    Adding my changes from Craytor's base

--- index.php
+++ index.php
@@ -1,63 +1,65 @@
 <?php
-    session_start();
-    $sites = array('acadia.team-radiant.com', 'bamboo.team-radiant.com', 'cosmos.team-radiant.com', 'droplet.team-radiant.com');
 
-    foreach($sites as $site) {
-        $_SESSION[$site] = json_decode(file_get_contents("http://" . $site . "/?json=1"));
-    }
+	include "sites.php";
 
+	$site_data = array();
+	foreach($sites as $site) {
+		$data = json_decode(file_get_contents("http://" . $site["domain"] . "/?json=1"));
+		$data->name = $site["name"];
+		$data->ip = gethostbyname($site["domain"]);
+		$site_data[] = $data;
+	}
 
 ?>
+<!doctype html>
 <html>
-    <head>
-        <title>Status</title>
-        <link rel="stylesheet" href="css/main.css" />
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="js/jqknob.js"></script>
-        <script>
-            $(document).ready(function() {
-                // Show ring charts
-                $("#k-disk, #k-memory, #k-swap, #k-cpu").knob({
-                    readOnly: true,
-                    width: 40,
-                    height: 40,
-                    thickness: 0.2,
-                    fontWeight: 'normal',
-                    bgColor: 'rgba(127,127,127,0.15)', // 50% grey with a low opacity, should work with most backgrounds
-                    fgColor: '#ccc'
-                });
-                // Start AJAX update loop
-                update();
-            });
-        </script>
-    </head>
-    <body>
-        <section class="header">
-            <h1>Server Status Page</h1>
-        </section>
-        <section class="list">
-            <?php foreach($sites as $site) { ?>
-
-            <div class="status" style="background-image: url('<?php echo $_SESSION[$site]->{'img'}; ?>'); background-size: 100%; background-position: center center;" >
-                <div class="grey">
-                    <div class="left">
-                        <h4><?php echo $site; ?></h4>
-                        <span><?php echo $_SESSION[$site]->{'ip'}; ?></span>
-                    </div>
-                    <div class="right">
-                        Uptime: <span id="uptime"><?php echo $_SESSION[$site]->{'uptime'}; ?></span>&emsp;
-                        Disk usage: <input id="k-disk" value="<?php echo $_SESSION[$site]->{'disk'}; ?>">&emsp;
-                        Memory: <input id="k-memory" value="<?php echo $_SESSION[$site]->{'memory'}; ?>">&emsp;
-                        <?php if($_SESSION[$site]->{'swap_total'} !== "0") { ?>
-                            Swap: <input id="k-swap" value="<?php echo $_SESSION[$site]->{'swap'}; ?>">&emsp;
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-
-            <?php } ?>
-
-
-        </section>
-    </body>
+	<head>
+		<meta charset="utf-8">
+		<title>Status</title>
+		<link rel="stylesheet" href="css/main.css" />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="js/jqknob.js"></script>
+		<script>
+			$(document).ready(function() {
+				// Show ring charts
+				$(".k-disk, .k-memory, .k-swap, .k-cpu").knob({
+					readOnly: true,
+					width: 40,
+					height: 40,
+					thickness: 0.2,
+					fontWeight: 'normal',
+					bgColor: 'rgba(127,127,127,0.15)', // 50% grey with a low opacity, should work with most backgrounds
+					fgColor: '#ccc'
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<section class="header">
+			<h1>Server Status Page</h1>
+		</section>
+		<section class="list">
+			<?php foreach($site_data as $data) { ?>
+				<!--
+<?php var_dump($data); ?>
+				-->
+				<div class="status">
+					<div class="grey">
+						<div class="left">
+							<h4><?php echo $data->name; ?></h4>
+							<span><?php echo $data->ip; ?></span>
+						</div>
+						<div class="right">
+							Uptime: <span class="uptime"><?php echo $data->uptime; ?></span>&emsp;
+							Disk usage: <input class="k-disk" value="<?php echo $data->disk; ?>">&emsp;
+							Memory: <input class="k-memory" value="<?php echo $data->memory; ?>">&emsp;
+							<?php if($data->swap_total) { ?>
+								Swap: <input class="k-swap" value="<?php echo $data->swap; ?>">&emsp;
+							<?php } ?>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</section>
+	</body>
 </html>
---
+++ sites.php
@@ -0,0 +1,20 @@
+<?php
+
+$sites = array(
+	array(
+		"domain" => 'hanako.alanaktion.net',
+		"name" => "Hanako"
+	),
+	array(
+		"domain" => 'mini-hanako.alanaktion.net',
+		"name" => "Mini Hanako"
+	),
+	array(
+		"domain" => 'iwanako.alanaktion.net',
+		"name" => "Iwanako"
+	),
+	array(
+		"domain" => 'sae.alanaktion.net',
+		"name" => "Sae"
+	),
+);
